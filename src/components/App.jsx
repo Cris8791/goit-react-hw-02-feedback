336f2b16e73ec0e86e9e697157a2b651b00264e4
Cris8791 <135622871+Cris8791@users.noreply.github.com>
2024-02-09 09:45:48 +0200

    commit1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,68 +1,47 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Statistics from './feedbacks/Statistics';
 import FeedbackOptions from './feedbacks/FeedbackOptions';
 import Section from './feedbacks/Section';
 import Notification from './feedbacks/Notification';
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  // Metodele pentru incrementarea feedback-ului
-  handleGood = () => {
-    this.setState(prevState => ({ good: prevState.good + 1 }));
-  };
+  const handleGood = () => setGood(good + 1);
+  const handleNeutral = () => setNeutral(neutral + 1);
+  const handleBad = () => setBad(bad + 1);
 
-  handleNeutral = () => {
-    this.setState(prevState => ({ neutral: prevState.neutral + 1 }));
-  };
+  return (
+    <div>
+      <Section title="Leave Feedback">
+        <FeedbackOptions
+          options={{
+            good: handleGood,
+            neutral: handleNeutral,
+            bad: handleBad,
+          }}
+        />
+      </Section>
 
-  handleBad = () => {
-    this.setState(prevState => ({ bad: prevState.bad + 1 }));
-  };
-
-  render() {
-    return (
-      <div>
-        <Section title="Leave Feedback">
-          <FeedbackOptions
-            options={{
-              good: this.handleGood,
-              neutral: this.handleNeutral,
-              bad: this.handleBad,
-            }}
+      <Section title="Statistics">
+        {good || neutral || bad ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={good + neutral + bad}
+            positivePercentage={
+              good ? Math.round((good / (good + neutral + bad)) * 100) : 0
+            }
           />
-        </Section>
-
-        <Section title="Statistics">
-          {this.state.good || this.state.neutral || this.state.bad ? (
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.state.good + this.state.neutral + this.state.bad}
-              positivePercentage={
-                this.state.good
-                  ? Math.round(
-                      (this.state.good /
-                        (this.state.good +
-                          this.state.neutral +
-                          this.state.bad)) *
-                        100
-                    )
-                  : 0
-              }
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </div>
+  );
+};
 
 export default App;
